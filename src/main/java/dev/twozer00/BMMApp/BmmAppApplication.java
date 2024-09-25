a19070fc0d17bbbc6ba14c30faddc50955f4eef4
Leobardo Verdugo <leobardo@twozer00.dev>
2024-09-25 12:15:24 -0700

    inital commit

---
+++ src/main/java/dev/twozer00/BMMApp/BmmAppApplication.java
@@ -0,0 +1,13 @@
+package dev.twozer00.BMMApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmmAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmmAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/twozer00/BMMApp/BmmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.twozer00.BMMApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
